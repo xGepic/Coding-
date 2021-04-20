1fc3f809e5293084d681414e3137ac3c93c5f7b9
Gepic <75324840+xGepic@users.noreply.github.com>
2021-04-21 00:51:25 +0200

    Create helpcenter.php

---
+++ 2. Sem/WebDev/Aurora/helpcenter.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Hilfe-Center-Seite
+ *
+*/
+?>
+
+<div class="row">
+						<div class="col-lg-12">
+							<div class="tab-content">
+								<div class="acc-setting">
+									<h3>Wilkommen zum Hilfe-Center!</h3>
+										<div class="notbar">
+											<h3>Durchsuchen Sie Unsere häufig gestellten Fragen.</h4>
+										</div>
+										<div class="notbar">
+											<h4>Wie kann ich mich registrieren?</h4>
+											<p>Um einen eigenen Account zu erstellen brauchen Sie eine E-Mail Adresse und einen Usernamen, mit denen noch kein Account erstellt wurde.</p>
+											<p>Nun können Sie das Registrierungs-Formular auf der Startseite ausfüllen und auf "Registrieren" klicken.</p>
+											<p>Sofern Sie alles richtig eingegeben haben können Sie Sich nun mit Ihrem neuen Account anmelden!</p>
+										</div>
+										<div class="notbar">
+											<h4>Wie logge ich mich ein?</h4>
+											<p>Einloggen können Sie sich mit Ihrer E-Mail Adresse und Ihrem Passwort, indem Sie das Login-Formular verwenden.</p>
+											<p>Dieses wird stets angezeigt.</p>
+										</div>
+										<div class="notbar">
+											<h4>Wie logge ich mich aus?</h4>
+											<p>Rechts oben ist ein kleines Icon mit Ihrem Profilbild und Usernamen.</p>
+											<p>Klicken Sie auf dieses. Eine Box wird darunter Aufklappen.</p>
+											<p>Nun können Sie auf "Logout" klicken und sich ausloggen.</p>
+										</div>
+										<div class="notbar">
+											<h4>Wie poste ich etwas?</h4>
+											<p>Sie gehen auf die Startseite wo alle Posts angezeigt werden.</p>
+											<p>Ganz oben sit ein Formular, wo sie Text und Tags ausfüllen und ein Bild hochladen können.</p>
+											<p>Außerdem können sie den Post auf private oder public einstellen.</p>
+											<p>Mit dem "Posten" Button wird ihr Post veröffentlicht.</p>
+										</div>
+										<div class="notbar">
+											<h4>Wie kann ich einen Post kommentieren?</h4>
+											<p>Um einen Post kommentieren zu können, müssen Sie in die Einzelanzeige gehen.</p>
+											<p>Dafür können Sie den Link im Datum verwenden - klicken Sie es an.</p>
+											<p>Unter dem Beitrag gibt es eine "Kommentieren" Box.</p>
+											<p>Füllen Sie diese aus und klicken auf "Posten" um Ihren kommentar zu veröffentlichen.</p>
+										</div>
+										<div class="notbar">
+											<h4>Bei weiteren Fragen wenden Sie sich gern über das Kontakt-Formular direkt an uns!</h4>
+										</div>
+								</div>
+							</div>
+						</div>
+					</div>
