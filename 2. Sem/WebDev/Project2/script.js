0226435ce7c172f54d2612cf63624b82c8732c9a
Gepic <75324840+xGepic@users.noreply.github.com>
2021-04-21 01:17:34 +0200

    Create script.js

---
+++ 2. Sem/WebDev/Project2/script.js
@@ -0,0 +1,12 @@
+function myFunction() {
+    
+    var table = document.getElementById("myTable");
+    var rowCount = table.rows.length;
+    var row = table.insertRow(rowCount);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    cell1.innerHTML = "NEW CELL1";
+    cell2.innerHTML = "NEW CELL2";
+    cell3.innerHTML = "NEW CELL3";
+}
