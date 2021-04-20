c0bba3731f2d12a08ab208861b5bb40def30255b
Gepic <75324840+xGepic@users.noreply.github.com>
2021-04-21 00:50:49 +0200

    Create databaseauthentication.php

---
+++ 2. Sem/WebDev/Aurora/databaseauthentication.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Wird direkt am Anfang im index aufgerufen - Informationen um sich mit der Datenbank zu verbinden.
+ *
+*/
+
+    $servername = 'localhost';
+    $dbname = 'aurora';
+    $username = 'root';
+    $password = '';
+?>
