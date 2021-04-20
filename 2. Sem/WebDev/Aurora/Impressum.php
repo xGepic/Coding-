f9fd53d1fbc3bd73e54b5f8bbf8fe562079c6596
Gepic <75324840+xGepic@users.noreply.github.com>
2021-04-21 00:51:37 +0200

    Create Impressum.php

---
+++ 2. Sem/WebDev/Aurora/Impressum.php
@@ -0,0 +1,151 @@
+<?php
+/**
+ * Kontakt und Impressum Seite
+ *
+ * Formular Schickt Nachricht per POST an definierte E-Mail Adresse.
+ *
+*/
+?>
+
+<form action="mailto:simanek.s@gmx.at" method="POST" enctype="text/plain">
+        <table class="table">
+            <tr>
+                <th>Beschriftung</th>
+                <th>Feld</th>
+            </tr>
+    <tr>
+        <td><label for="firma">Firma: </label></td>
+        <td><input id="firma" type="text" name="firma" placeholder="Musterfirma"></td>
+    </tr>
+    <tr>
+        <td><label for="anrede">Anrede/Titel: </label></td>
+        <td><input id="anrede" type="text" name="anrede"></td>
+    </tr>
+    <tr>
+        <td><label for="vorname">Vorname: </label></td>
+        <td><input id="vorname" type="text" name="vorname" required></td>
+    </tr>
+    <tr>
+        <td><label for="nachname">Nachname: </label></td>
+        <td><input id="nachname" type="text" name="nachname" required></td>
+    </tr>
+    <tr>
+        <td><label for="email">Email: </label></td>
+        <td><input id="email" type="email" name="email" required></td>
+    </tr>
+    <tr>
+        <td><label for="telefon">Telefon: </label></td>
+        <td><input type ="number" name ="telefon" id="telefon" required></td>
+    </tr>
+    <tr>
+        <td><label for="betreff">Betreff: </label></td>
+        <td><input type ="text" name ="betreff" id="betreff" required></td>
+    </tr>
+    <tr>
+        <td><label for="nachricht">Nachricht: </label></td>
+        <td> <textarea name="nachricht" id="nachricht" cols="30" rows="10" ></textarea></td>
+    </tr>
+    <tr>
+    <td>
+        <button class="btn btn-outline-dark btn-sm" type="submit" value="Absenden">Absenden</button>
+        </td>
+        <td>
+        <button class="btn btn-outline-dark btn-sm" type="reset" value="Abbrechen">Abbrechen</button>
+        </td>
+    </tr>
+    </table>
+    </form>
+
+
+<h1 class="adsimple-221140219">Impressum</h1>
+<p class="adsimple-221140219">Informationspflicht laut §5 E-Commerce Gesetz, §14 Unternehmensgesetzbuch, §63 Gewerbeordnung und Offenlegungspflicht laut §25 Mediengesetz.</p>
+<p class="adsimple-221140219">Aurora Entertainment<br />Matilda Bilaver, Christian Geßner & Stefan Simanek<br />FH Technikum Wien Höchstädtplatz 6,<br /> 1200 Wien, <br />Österreich</p>
+<p class="adsimple-221140219">
+<strong>Unternehmensgegenstand:</strong> IT Dienstleistungen<br />
+<strong>UID-Nummer:</strong> ATU12345678<br />
+</p>
+<p class="adsimple-221140219">
+<strong>Tel.:</strong> 01234/56789<br />
+<strong>Fax:</strong> 01234/56789-0<br />
+<strong>E-Mail:</strong> <a href="mailto:if20b180@technikum-wien.at">Matilda Bilaver if20b180@technikum-wien.at</a><br />
+<strong>E-Mail:</strong> <a href="mailto:if20b022@technikum-wien.at">Christian Geßner if20b180@technikum-wien.at</a><br />
+<strong>E-Mail:</strong> <a href="mailto:if20b015@technikum-wien.at">Stefan Simanek if20b180@technikum-wien.at</a>
+</p>
+<p class="adsimple-221140219">
+<strong>Mitglied bei:</strong> WKO, Landesinnung, etc.<br />
+<strong>Berufsrecht:</strong> Gewerbeordnung: www.ris.bka.gv.at</p>
+<p class="adsimple-221140219">
+<strong>Aufsichtsbehörde/Gewerbebehörde:</strong> Bezirkshauptmannschaft Musterhausen<br />
+<strong>Berufsbezeichnung:</strong> Webdesigner, Grafiker<br />
+<strong>Verleihungsstaat:</strong> Österreich</p><br />
+<h2 class="adsimple-221140219">EU-Streitschlichtung</h2>
+<p>Gemäß Verordnung über Online-Streitbeilegung in Verbraucherangelegenheiten (ODR-Verordnung) möchten wir Sie über die Online-Streitbeilegungsplattform (OS-Plattform) informieren.<br />
+Verbraucher haben die Möglichkeit, Beschwerden an die Online Streitbeilegungsplattform der Europäischen Kommission unter <a class="adsimple-221140219" href="https://ec.europa.eu/consumers/odr/main/index.cfm?event=main.home2.show&lng=DE"  target="_blank"  class="external" rel="nofollow">http://ec.europa.eu/odr?tid=221140219</a> zu richten. Die dafür notwendigen Kontaktdaten finden Sie oberhalb in unserem Impressum.</p>
+<p>Wir möchten Sie jedoch darauf hinweisen, dass wir nicht bereit oder verpflichtet sind, an Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teilzunehmen.</p>
+<h2 class="adsimple-221140219">Haftung für Inhalte dieser Webseite</h2>
+<p>Wir entwickeln die Inhalte dieser Webseite ständig weiter und bemühen uns korrekte und aktuelle Informationen bereitzustellen. Leider können wir keine Haftung für die Korrektheit aller Inhalte auf dieser Webseite übernehmen, speziell für jene die seitens Dritter bereitgestellt wurden.</p>
+<p>Sollten Ihnen problematische oder rechtswidrige Inhalte auffallen, bitten wir Sie uns umgehend zu kontaktieren, Sie finden die Kontaktdaten im Impressum.</p>
+<h2 class="adsimple-221140219">Haftung für Links auf dieser Webseite</h2>
+<p>Unsere Webseite enthält Links zu anderen Webseiten für deren Inhalt wir nicht verantwortlich sind. Haftung für verlinkte Websites besteht laut <a class="adsimple-221140219" href="https://www.ris.bka.gv.at/Dokument.wxe?Abfrage=Bundesnormen&Dokumentnummer=NOR40025813&tid=221140219" target="_blank" rel="noopener nofollow" class="external">§ 17 ECG</a> für uns nicht, da wir keine Kenntnis rechtswidriger Tätigkeiten hatten und haben, uns solche Rechtswidrigkeiten auch bisher nicht aufgefallen sind und wir Links sofort entfernen würden, wenn uns Rechtswidrigkeiten bekannt werden.</p>
+<p>Wenn Ihnen rechtswidrige Links auf unserer Website auffallen, bitten wir Sie uns zu kontaktieren, Sie finden die Kontaktdaten im Impressum.</p>
+<h2 class="adsimple-221140219">Urheberrechtshinweis</h2>
+<p>Alle Inhalte dieser Webseite (Bilder, Fotos, Texte, Videos) unterliegen dem Urheberrecht. Falls notwendig, werden wir die unerlaubte Nutzung von Teilen der Inhalte unserer Seite rechtlich verfolgen.</p>
+<h2 class="adsimple-221140219">Bildernachweis</h2>
+<p>Die Bilder, Fotos und Grafiken auf dieser Webseite sind urheberrechtlich geschützt.</p>
+<p>Die Bilderrechte liegen bei den folgenden Fotografen und Unternehmen:</p>
+<ul class="adsimple-221140219">
+<li class="adsimple-221140219">Fotograf Mustermann</li>
+</ul>
+<h1 class="adsimple-221140219">Datenschutzerklärung</h1>
+<h2 class="adsimple-221140219">Datenschutz</h2>
+<p>Wir haben diese Datenschutzerklärung (Fassung 23.01.2021-221140219) verfasst, um Ihnen gemäß der Vorgaben der <a class="adsimple-221140219" href="https://eur-lex.europa.eu/legal-content/DE/ALL/?uri=celex%3A32016R0679&tid=221140219" target="_blank" rel="noopener noreferrer nofollow" class="external">Datenschutz-Grundverordnung (EU) 2016/679</a> zu erklären, welche Informationen wir sammeln, wie wir Daten verwenden und welche Entscheidungsmöglichkeiten Sie als Besucher dieser Webseite haben.</p>
+<p>Leider liegt es in der Natur der Sache, dass diese Erklärungen sehr technisch klingen, wir haben uns bei der Erstellung jedoch bemüht die wichtigsten Dinge so einfach und klar wie möglich zu beschreiben.</p>
+<h2 class="adsimple-221140219">Automatische Datenspeicherung</h2>
+<p>Wenn Sie heutzutage Webseiten besuchen, werden gewisse Informationen automatisch erstellt und gespeichert, so auch auf dieser Webseite.</p>
+<p>Wenn Sie unsere Webseite so wie jetzt gerade besuchen, speichert unser Webserver (Computer auf dem diese Webseite gespeichert ist) automatisch Daten wie</p>
+<ul class="adsimple-221140219">
+<li class="adsimple-221140219">die Adresse (URL) der aufgerufenen Webseite</li>
+<li class="adsimple-221140219">Browser und Browserversion</li>
+<li class="adsimple-221140219">das verwendete Betriebssystem</li>
+<li class="adsimple-221140219">die Adresse (URL) der zuvor besuchten Seite (Referrer URL)</li>
+<li class="adsimple-221140219">den Hostname und die IP-Adresse des Geräts von welchem aus zugegriffen wird</li>
+<li class="adsimple-221140219">Datum und Uhrzeit</li>
+</ul>
+<p>in Dateien (Webserver-Logfiles).</p>
+<p>In der Regel werden Webserver-Logfiles zwei Wochen gespeichert und danach automatisch gelöscht. Wir geben diese Daten nicht weiter, können jedoch nicht ausschließen, dass diese Daten beim Vorliegen von rechtswidrigem Verhalten eingesehen werden.</p>
+<h2 class="adsimple-221140219">Speicherung persönlicher Daten</h2>
+<p>Persönliche Daten, die Sie uns auf dieser Website elektronisch übermitteln, wie zum Beispiel Name, E-Mail-Adresse, Adresse oder andere persönlichen Angaben im Rahmen der Übermittlung eines Formulars oder Kommentaren im Blog, werden von uns gemeinsam mit dem Zeitpunkt und der IP-Adresse nur zum jeweils angegebenen Zweck verwendet, sicher verwahrt und nicht an Dritte weitergegeben.</p>
+<p>Wir nutzen Ihre persönlichen Daten somit nur für die Kommunikation mit jenen Besuchern, die Kontakt ausdrücklich wünschen und für die Abwicklung der auf dieser Webseite angebotenen Dienstleistungen und Produkte. Wir geben Ihre persönlichen Daten ohne Zustimmung nicht weiter, können jedoch nicht ausschließen, dass diese Daten beim Vorliegen von rechtswidrigem Verhalten eingesehen werden.</p>
+<p>Wenn Sie uns persönliche Daten per E-Mail schicken – somit abseits dieser Webseite – können wir keine sichere Übertragung und den Schutz Ihrer Daten garantieren. Wir empfehlen Ihnen, vertrauliche Daten niemals unverschlüsselt per E-Mail zu übermitteln.</p>
+<h2 class="adsimple-221140219">Rechte laut Datenschutzgrundverordnung</h2>
+<p>Ihnen stehen laut den Bestimmungen der DSGVO und des österreichischen <a class="adsimple-221140219" href="https://www.ris.bka.gv.at/GeltendeFassung.wxe?Abfrage=Bundesnormen&Gesetzesnummer=10001597&tid=221140219" target="_blank" rel="noopener noreferrer nofollow" class="external">Datenschutzgesetzes (DSG)</a> grundsätzlich die folgende Rechte zu:</p>
+<ul class="adsimple-221140219">
+<li class="adsimple-221140219">Recht auf Berichtigung (Artikel 16 DSGVO)</li>
+<li class="adsimple-221140219">Recht auf Löschung („Recht auf Vergessenwerden“) (Artikel 17 DSGVO)</li>
+<li class="adsimple-221140219">Recht auf Einschränkung der Verarbeitung (Artikel 18 DSGVO)</li>
+<li class="adsimple-221140219">Recht auf Benachrichtigung – Mitteilungspflicht im Zusammenhang mit der Berichtigung oder Löschung personenbezogener Daten oder der Einschränkung der Verarbeitung (Artikel 19 DSGVO)</li>
+<li class="adsimple-221140219">Recht auf Datenübertragbarkeit (Artikel 20 DSGVO)</li>
+<li class="adsimple-221140219">Widerspruchsrecht (Artikel 21 DSGVO)</li>
+<li class="adsimple-221140219">Recht, nicht einer ausschließlich auf einer automatisierten Verarbeitung — einschließlich Profiling — beruhenden Entscheidung unterworfen zu werden (Artikel 22 DSGVO)</li>
+</ul>
+<p>Wenn Sie glauben, dass die Verarbeitung Ihrer Daten gegen das Datenschutzrecht verstößt oder Ihre datenschutzrechtlichen Ansprüche sonst in einer Weise verletzt worden sind, können Sie sich bei der Aufsichtsbehörde beschweren, welche in Österreich die Datenschutzbehörde ist, deren Webseite Sie unter <a class="adsimple-221140219" href="https://www.dsb.gv.at/?tid=221140219" class="external" rel="nofollow">https://www.dsb.gv.at/</a> finden.</p>
+<h2 class="adsimple-221140219">TLS-Verschlüsselung mit https</h2>
+<p>Wir verwenden https um Daten abhörsicher im Internet zu übertragen (Datenschutz durch Technikgestaltung <a class="adsimple-221140219" href="https://eur-lex.europa.eu/legal-content/DE/TXT/HTML/?uri=CELEX:32016R0679&from=DE&tid=221140219" target="_blank" rel="noopener noreferrer nofollow" class="external">Artikel 25 Absatz 1 DSGVO</a>). Durch den Einsatz von TLS (Transport Layer Security), einem Verschlüsselungsprotokoll zur sicheren Datenübertragung im Internet können wir den Schutz vertraulicher Daten sicherstellen. Sie erkennen die Benutzung dieser Absicherung der Datenübertragung am kleinen Schlosssymbol links oben im Browser und der Verwendung des Schemas https (anstatt http) als Teil unserer Internetadresse.</p>
+<h2 class="adsimple-221140219">jQuery CDN Datenschutzerklärung</h2>
+<p>Um Ihnen unsere Website bzw. all unsere einzelnen Unterseiten (Webseiten) auf unterschiedlichen Geräten schnell und problemlos auszuliefern, nutzen wir Dienste von jQuery CDN des Unternehmens jQuery Foundation. jQuery wird über das Content Delivery Network (CDN) des amerikanischen Software-Unternehmens StackPath (LCC 2012 McKinney Ave. Suite 1100, Dallas, TX 75201, USA) verteilt. Durch diesen Dienst werden personenbezogene Daten von Ihnen gespeichert, verwaltet und verarbeitet.</p>
+<p>Ein Content Delivery Network (CDN) ist ein Netzwerk regional verteilter Server, die über das Internet miteinander verbunden sind. Durch dieses Netzwerk können Inhalte, speziell sehr große Dateien, auch bei großen Lastspitzen schnell ausgeliefert werden.</p>
+<p>jQuery nutzt JavaScript-Bibliotheken, um unsere Website-Inhalte zügig ausliefern zu können. Dafür lädt ein CDN-Server die nötigen Dateien. Sobald eine Verbindung zum CDN-Server aufgebaut ist, wird Ihre IP-Adresse erfasst und gespeichert. Das geschieht nur, wenn diese Daten nicht schon durch einen vergangenen Websitebesuch in Ihrem Browser gespeichert sind.</p>
+<p>In den Datenschutz-Richtlinien von StackPath wird ausdrücklich erwähnt, dass StackPath aggregierte und anonymisierte Daten von diversen Diensten (wie eben auch jQuery) für die Erweiterung der Sicherheit und für eigene Dienste benutzen. Diese Daten können Sie als Person allerdings nicht identifizieren.</p>
+<p>Wenn Sie nicht wollen, dass es zu dieser Datenübertragung kommt, haben Sie immer auch die Möglichkeit Java-Scriptblocker wie beispielsweise <a class="adsimple-221140219" href="https://www.ghostery.com/de/" target="_blank" rel="follow noopener noreferrer" class="external">ghostery.com</a> oder <a class="adsimple-221140219" href="https://noscript.net/" target="_blank" rel="follow noopener noreferrer" class="external">noscript.net</a> zu installieren. Sie können aber auch einfach in Ihrem Browser die Ausführung von JavaScript-Codes deaktivieren. Wenn Sie sich für die Deaktivierung von JavaScript-Codes entscheiden, verändern sich auch die gewohnten Funktionen. So wird beispielsweise eine Website nicht mehr so schnell geladen.</p>
+<p>StackPath ist aktiver Teilnehmer beim EU-U.S. Privacy Shield Framework, wodurch der korrekte und sichere Datentransfer persönlicher Daten geregelt wird. Mehr Informationen dazu finden Sie auf <a class="adsimple-221140219" href="https://www.privacyshield.gov/participant?id=a2zt0000000CbahAAC&status=Active" target="_blank" rel="follow noopener noreferrer" class="external">https://www.privacyshield.gov/participant?id=a2zt0000000CbahAAC&status=Active</a>.<br />
+Mehr Informationen zum Datenschutz bei StackPath finden Sie unter <a class="adsimple-221140219" href="https://www.stackpath.com/legal/privacy-statement/?tid=221140219" target="_blank" rel="noopener noreferrer nofollow" class="external">https://www.stackpath.com/legal/privacy-statement/</a> und zu jQuery unter <a class="adsimple-221140219" href="https://openjsf.org/wp-content/uploads/sites/84/2019/11/OpenJS-Foundation-Privacy-Policy-2019-11-15.pdf" target="_blank" rel="follow noopener noreferrer" class="external">https://openjsf.org/wp-content/uploads/sites/84/2019/11/OpenJS-Foundation-Privacy-Policy-2019-11-15.pdf</a>.</p>
+<h2 class="adsimple-221140219">BootstrapCDN Datenschutzerklärung</h2>
+<p>Um Ihnen all unsere einzelnen Webseiten (Unterseiten unserer Website) auf allen Geräten schnell und sicher ausliefern zu können, nutzen wir das Content Delivery Network (CDN) BootstrapCDN des amerikanischen Software-Unternehmens StackPath, LLC 2012 McKinney Ave. Suite 1100, Dallas, TX 75201, USA.</p>
+<p>Ein Content Delivery Network (CDN) ist ein Netzwerk regional verteilter Server, die über das Internet miteinander verbunden sind. Durch dieses Netzwerk können Inhalte, speziell sehr große Dateien, auch bei großen Lastspitzen schnell ausgeliefert werden.</p>
+<p>BootstrapCDN funktioniert so, dass sogenannte JavaScript-Bibliotheken an Ihren Browser ausgeliefert werden. Lädt nun Ihr Browser eine Datei vom BootstrapCDN herunter, wird Ihre IP-Adresse während der Verbindung zum Bootstrap-CDN-Server an das Unternehmen StockPath übermittelt.</p>
+<p>StackPath erwähnt auch in der hauseigenen Datenschutzerklärung, dass das Unternehmen aggregierte und anonymisierte Daten von diversen Diensten (wie BootstrapCDN) für die Erweiterung der Sicherung und für andere StackPath-Dienste und Clients verwenden. All diese Daten können aber keine Person identifizieren.</p>
+<p>Wenn Sie diese Datenübertragung unterbinden wollen, können Sie einen JavaScript-Blocker (siehe beispielsweise <a class="adsimple-221140219" href="https://noscript.net/" target="_blank" rel="noopener noreferrer nofollow" class="external">https://noscript.net/</a>) installieren oder in Ihrem Browser die Ausführung von JavaScript-Codes deaktivieren. Bitte beachten Sie aber, dass dadurch die Website nicht mehr das gewohnte Service (wie etwa schnelle Ladegeschwindigkeit) bieten kann.</p>
+<p>StackPath ist aktiver Teilnehmer beim EU-U.S. Privacy Shield Framework, wodurch der korrekte und sichere Datentransfer persönlicher Daten geregelt wird. Mehr Informationen dazu finden Sie auf <a class="adsimple-221140219" href="https://www.privacyshield.gov/participant?id=a2zt0000000CbahAAC&status=Active" target="_blank" rel="follow noopener noreferrer" class="external">https://www.privacyshield.gov/participant?id=a2zt0000000CbahAAC&status=Active</a>.<br />
+Mehr Informationen zum Datenschutz bei StackPath bzw. BootstrapCDN finden Sie auf <a class="adsimple-221140219" href="https://www.bootstrapcdn.com/privacy-policy/?tid=221140219" target="_blank" rel="noopener noreferrer nofollow" class="external">https://www.bootstrapcdn.com/privacy-policy/</a>.</p>
+<p style="margin-top:15px;">Quelle: Erstellt mit dem <a title="Datenschutz Generator von firmenwebseiten.at" href="https://www.firmenwebseiten.at/datenschutz-generator/" target="_blank" rel="follow" style="text-decoration:none;">Datenschutz Generator</a> von firmenwebseiten.at in Kooperation mit <a href="https://www.tattooentfernen.at" target="_blank" rel="follow" title="" style="text-decoration:none;">tattooentfernen.at</a>
+</p>
