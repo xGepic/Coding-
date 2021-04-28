f4364bae3715bc3e451a1607eeeceb2a926fe73c
Gepic <75324840+xGepic@users.noreply.github.com>
2021-04-29 01:43:28 +0200

    Update dataHandler.php

--- 2. Sem/WebDev/Project2/dataHandler.php
+++ 2. Sem/WebDev/Project2/dataHandler.php
@@ -1,6 +1,7 @@
 <?php
 include("models/appointment.php");
 include("models/dateTime.php");
+include("models/vote.php");
 include("db.php");
 class DataHandler
 {
@@ -30,7 +31,6 @@ class DataHandler
         $ergebnis = $this->db->queryDatesByAppointment($idAppointment);
         while ($row = mysqli_fetch_object($ergebnis)) {
             $dateTime = new DateAndTime($row->date, $row->time);
-            $dateTime->setID($row->id);
             array_push($array, $dateTime);
         }
         return $array;
@@ -52,11 +52,8 @@ class DataHandler
         $pieces=explode(",",$param);
         for($i=0;$i<5;$i++)
         {
-            echo $pieces[$i*2+1];
-            echo $pieces[($i+1)*2];
             $dateTime=new DateAndTime($pieces[$i*2+1],$pieces[($i+1)*2]);
             array_push($array, $dateTime);
-            
         }
         if($this->db->queryPostDateTimeArray($pieces[0],$array))
         {
@@ -71,22 +68,27 @@ class DataHandler
     public function queryBookAppointment($param)
     {
         $pieces=explode(",",$param);
-        return $this->db->queryBookAppointment($pieces[0],$pieces[1]);
-    }
-
-    public function queryUnbookAppointment($param)
-    {
-        return $this->db->queryUnbookAppointment($param);
-    }
-
-    public function queryIsItBooked($param)
-    {
-        $ergebnis=$this->db->queryIsItBooked($param);
+        $ergebnis = $this->db->queryBookAppointment($pieces[0],$pieces[1],$pieces[2]);
         $row = mysqli_fetch_object($ergebnis);
-        if($row->nameVonBenutzer==NULL)
-            return 0;
-        else
-            return 1;
+        return $row;
+    }
+
+    public function queryDeleteAppointment($param)
+    {
+        return $this->db->queryDeleteAppointment($param);
+    }
+
+    public function queryVotingsByAppointment($param)
+    {
+        $array = array();
+        $ergebnis=$this->db->queryVotingsByAppointment($param);
+        while ($row = mysqli_fetch_object($ergebnis)) {
+            $vote = new Vote($row->name, $row->comment);
+            $dateTime=new DateAndTime($row->date,$row->time);
+            array_push($array, $vote);
+            array_push($array, $dateTime);
+        }
+        return $array;
     }
 
     function getDB() { return $this->db; }
