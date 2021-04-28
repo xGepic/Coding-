1e6ce7ea525b3a269d08653409e04f4a80b79e7d
Gepic <75324840+xGepic@users.noreply.github.com>
2021-04-29 01:47:54 +0200

    Update Main.cpp

--- 2. Sem/OOP/Deepminer/Main.cpp
+++ 2. Sem/OOP/Deepminer/Main.cpp
@@ -6,12 +6,16 @@ using namespace std;
 
 int main() {
 
-	srand((unsigned int)time(NULL));
+	srand((unsigned int)time(nullptr));
 
 	World* myWorld = new World();
+	myWorld->getMenu();
 
+	Bot* Player1 = myWorld->chooseBot();
+	Bot* Robot = myWorld->chooseRobot();
 
+	myWorld->startGame(Player1, Robot);
 
 
 	delete myWorld;
-}
+} 
