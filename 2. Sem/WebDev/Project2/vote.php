fba7f13c1e6a5b24a8574c4cac4de0a8ff8a9a00
Gepic <75324840+xGepic@users.noreply.github.com>
2021-04-29 01:44:45 +0200

    Create vote.php

---
+++ 2. Sem/WebDev/Project2/vote.php
@@ -0,0 +1,17 @@
+<?php
+class Vote {
+    public $id;
+    public $nameVonBenutzer;
+    public $comment;
+
+    function __construct($nameVonBenutzer, $comment) {
+        $this->nameVonBenutzer = $nameVonBenutzer;
+        $this->comment = $comment;
+      }
+      public function getID() { return $this->id; }
+      public function setID($id) { $this->id=$id; }
+      public function getNameVonBenutzer() { return $this->nameVonBenutzer; }
+      public function setNameVonBenutzer($nameVonBenutzer) { $this->nameVonBenutzer=$nameVonBenutzer; }
+      public function getComment() { return $this->comment; }
+      public function setComment($comment) { $this->comment=$comment; }
+}
