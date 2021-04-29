57ab346695e7303ee1ca4e76906f747057e09917
Gepic <75324840+xGepic@users.noreply.github.com>
2021-04-29 13:17:10 +0200

    Create face.c

---
+++ 1. Sem/face.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("0123456789012345\n");
+    printf("________________\n");
+    printf("\\              /\n");
+    printf(" \\  \"\"\"  \"\"\"  /\n");
+    printf("  \\  o    o  /\n");
+    printf("   \\   __   /\n");
+    printf("    \\      /\n");
+    printf("     \\====/\n");
+    printf("      \\==/\n");
+    printf("       \\/\n");
+
+    return 0;
+}
