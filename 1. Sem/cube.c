2e573a7acad5dfbad6ee5c31954bbee43909f223
Gepic <75324840+xGepic@users.noreply.github.com>
2021-04-29 13:16:24 +0200

    Create cube.c

---
+++ 1. Sem/cube.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+int A;
+int V;
+int s = 0;
+
+printf(": ");
+scanf("%d", &s);
+
+A = 6*s*s;
+V = s*s*s;
+
+printf("Surface area: %d\n",A);
+printf("Volume: %d", V);
+
+}
