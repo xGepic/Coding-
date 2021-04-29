6ced80edd7f4c4cf1364f3552ba9267c2060b5a6
Gepic <75324840+xGepic@users.noreply.github.com>
2021-04-29 13:20:53 +0200

    Create Tax.c

---
+++ 1. Sem/Tax.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    double b = 0;
+    double t;
+
+    printf(": ");
+    scanf("%lf",&b);
+
+    t = 1.2*b;
+
+    printf("With tax: %.2lf",t);
+
+}
