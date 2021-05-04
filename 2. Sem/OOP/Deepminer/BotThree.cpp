2b76152c28a2c14365ce2d2889a98ac8acb910e2
Gepic <75324840+xGepic@users.noreply.github.com>
2021-05-05 00:40:29 +0200

    Update BotThree.cpp

--- 2. Sem/OOP/Deepminer/BotThree.cpp
+++ 2. Sem/OOP/Deepminer/BotThree.cpp
@@ -9,7 +9,15 @@ BotThree::~BotThree() {
 	delete this;
 }
 
-void BotThree::mine(int depth[10]) {
+void BotThree::mine(vector<int> depth) {
 
+	int value = 0;
 
+	reverse(depth.begin(), depth.end());
+
+	value += depth.front();
+	depth.erase(depth.begin());
+	value += depth.front();
+
+	addScore(value);
 }
