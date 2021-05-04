3ceed31b7b5ee1bd5b39a20391190381085b6071
Gepic <75324840+xGepic@users.noreply.github.com>
2021-05-05 00:40:19 +0200

    Update BotTwo.cpp

--- 2. Sem/OOP/Deepminer/BotTwo.cpp
+++ 2. Sem/OOP/Deepminer/BotTwo.cpp
@@ -9,7 +9,18 @@ BotTwo::~BotTwo() {
 	delete this;
 }
 
-void BotTwo::mine(int depth[10]) {
+void BotTwo::mine(vector<int> depth) {
+	
+	int value = 0;
 
+	depth.erase(depth.begin());
+	value += depth.front(); 
 
+	depth.erase(depth.begin());
+	value += depth.front(); 
+
+	depth.erase(depth.begin());
+	value += depth.front();  
+
+	addScore(value);
 }
