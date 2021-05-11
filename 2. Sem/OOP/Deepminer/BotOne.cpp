e9281f788d38f1abdfcd4bc9b8d58d9ac86d6549
Gepic <75324840+xGepic@users.noreply.github.com>
2021-05-11 18:00:52 +0200

    Update BotOne.cpp

--- 2. Sem/OOP/Deepminer/BotOne.cpp
+++ 2. Sem/OOP/Deepminer/BotOne.cpp
@@ -12,9 +12,16 @@ BotOne::~BotOne() {
 void BotOne::mine(vector<int> depth) {
 
 	int value = 0;
+	int counter = count(depth.begin(), depth.end(), 4);
 
 	sort(depth.begin(), depth.end());
 	depth.pop_back();
 	value = depth.back();
+
+	if (counter > 0) {
+		
+		addExtra(counter);
+	}
+
 	addScore(value);
 }
