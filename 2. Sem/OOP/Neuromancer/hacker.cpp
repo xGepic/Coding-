71739da79aec864e56bfe5e5aa97bdd73ff54719
Gepic <75324840+xGepic@users.noreply.github.com>
2021-05-29 18:08:17 +0200

    Update hacker.cpp

--- 2. Sem/OOP/Neuromancer/hacker.cpp
+++ 2. Sem/OOP/Neuromancer/hacker.cpp
@@ -13,14 +13,39 @@ hacker::~hacker() {
 
 }
 
+int hacker::getAttackPoints() {
+
+	return this->attackPoints;
+}
+
+int hacker::getLifePoints() {
+
+	return this->lifePoints;
+}
+
+int hacker::getScore() {
+
+	return this->score;
+}
+
+void hacker::changeScore(int x) {
+
+	score += x;
+}
+
 void hacker::changeLifePoints(int x) {
 
 	lifePoints += x;
 }
 
+void hacker::changeAttackPoints(int x) {
+
+	attackPoints += x;
+}
+
 void hacker::attack(int whichZone) {
 
-	server* zone;
+	server* zone = nullptr;
 	int fail = rand() % 100 + 1;
 
 	if (whichZone == 1) {
@@ -51,5 +76,44 @@ void hacker::attack(int whichZone) {
 		return;
 	}
 
+	if ((this->getAttackPoints() + zone->getPressureValue()) > zone->getDefensePoints()) {
 
+		cout << "Attack Successful!" << endl;
+
+		int diff = (this->getAttackPoints() + zone->getPressureValue()) - zone->getDefensePoints();
+
+		zone->changeLifePoints(-1);
+		zone->setZero();
+		zone->changeDefensePoints(3);
+
+		for (int i = 0; i < diff; i++) {
+
+			//to do 
+		}
+		this->changeAttackPoints(1);
+		this->changeScore(1);
+	}
+	else {
+
+		cout << "Attack Failed!" << endl;
+		this->changeLifePoints(-1);
+
+		if ((this->getLifePoints() % 10) == 0) {
+
+			this->changeAttackPoints(-2);
+			zone->changePressureValue(1);
+		}
+	}
+
+	if (this->getLifePoints() <= 0) {
+		
+		cout << "Hacker Dead!" << endl;
+		return;
+	}
+
+	if (zone->getDefensePoints() <= 0) {
+
+		cout << "Server Dead!" << endl;
+		return;
+	}
 }
