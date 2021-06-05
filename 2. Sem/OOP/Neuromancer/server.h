da446af11044044f9574d08083007d16e059091d
Gepic <75324840+xGepic@users.noreply.github.com>
2021-06-05 21:19:53 +0200

    Update server.h

--- 2. Sem/OOP/Neuromancer/server.h
+++ 2. Sem/OOP/Neuromancer/server.h
@@ -1,87 +1,33 @@
 #pragma once
 #include <string>
+#include <vector>
 
 using namespace std;
 
-class server{
+struct zonen {
+
+	string name;
+	int defensePoints;
+	int pressureValue;
+};
+
+class server {
 
 private:
 	int lifePoints;
-	int defensePoints;
-	int pressureValue;
-	
-public:
-	string name;
+	vector<zonen> zonenArray;
 
+public:
 
 	server();
-	virtual ~server();
+	~server();
 
-	int getDefensePoints();
-	int getPressureValue();
 	int getLifePoints();
-	string getName();
+	int getZoneDefPoints(int whichZone);
+	int getZonePressureVal(int whichZone);
 
-	void changeLifePoints(int x);
-	void changeDefensePoints(int x);
-	void changePressureValue(int x);
-
-	void setZero();
-
-	virtual void beAttacked() = 0;
-};
-
-class defenseZone1 : public server {
-
-private:
-
-
-public:
-	defenseZone1();
-	virtual ~defenseZone1();
-	void beAttacked();
-};
-
-class defenseZone2 : public server {
-
-private:
-
-
-public:
-	defenseZone2();
-	virtual ~defenseZone2();
-	void beAttacked();
-};
-
-class defenseZone3 : public server {
-
-private:
-
-
-public:
-	defenseZone3();
-	virtual ~defenseZone3();
-	void beAttacked();
-};
-
-class defenseZone4 : public server {
-
-private:
-
-
-public:
-	defenseZone4();
-	virtual ~defenseZone4();	
-	void beAttacked();
-};
-
-class defenseZone5 : public server {
-
-private:
-
-
-public:
-	defenseZone5();
-	virtual ~defenseZone5();
-	void beAttacked();
+	void lifePointsMinusOne();
+	void pressureValSetZero(int whichZone);
+	void give3DefPoints(int whichZone);
+	void incPressureVal(int whichZone);
 };
