69ae1f89ddaf4b9b7e4d63b8cb46e6589ff54381
Gepic <75324840+xGepic@users.noreply.github.com>
2021-06-05 21:20:18 +0200

    Update main.cpp

--- 2. Sem/OOP/Neuromancer/main.cpp
+++ 2. Sem/OOP/Neuromancer/main.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
-#include <mutex>
-#include <future>
 #include "hacker.h"
 #include "server.h"
 
@@ -10,33 +8,9 @@ using namespace std;
 
 mutex m;
 
-server* rng() {
+int rng() {
 
-	int temp = rand() % 5 + 1;
-
-	if (temp == 1) {
-
-		return new defenseZone1();
-	}
-	if (temp == 2) {
-
-		return new defenseZone2();
-	}
-	if (temp == 3) {
-
-		return new defenseZone3();
-	}
-	if (temp == 4) {
-
-		return new defenseZone4();
-	}
-	if (temp == 5) {
-
-		return new defenseZone5();
-	}
-	else {
-		return nullptr;
-	}
+	return rand() % 5;
 }
 
 int main() {
@@ -46,25 +20,39 @@ int main() {
 	srand((unsigned int)time(NULL));
 	auto startTime = chrono::steady_clock::now();
 
+	server* Server = new server();
 	hacker* h1 = new hacker();
 	hacker* h2 = new hacker();
 	hacker* h3 = new hacker();
 	hacker* h4 = new hacker();
 	hacker* h5 = new hacker();
 
-	thread hacker1(&hacker::attack, h1, rng());
-	thread hacker2(&hacker::attack, h2, rng());
-	thread hacker3(&hacker::attack, h3, rng());
-	thread hacker4(&hacker::attack, h4, rng());
-	thread hacker5(&hacker::attack, h5, rng());
+	thread hacker1(&hacker::attack, h1, ref(Server), rng());
+	thread hacker2(&hacker::attack, h2, ref(Server), rng());
+	thread hacker3(&hacker::attack, h3, ref(Server), rng());
+	thread hacker4(&hacker::attack, h4, ref(Server), rng());
+	thread hacker5(&hacker::attack, h5, ref(Server), rng());
 	hacker1.join();
 	hacker2.join();
 	hacker3.join();
 	hacker4.join();
 	hacker5.join();
 
+	if ((h1->getLifePoints() <= 0) && (h2->getLifePoints() <= 0) && (h3->getLifePoints() <= 0) && (h4->getLifePoints() <= 0) && (h5->getLifePoints() <= 0)) {
+
+		cout << endl << "Server Won!" << endl;
+	}
+
+	if (Server->getLifePoints() <= 0) {
+
+		cout << endl << "Hackers Won!" << endl;
+	}
+
+	int sum = h1->getScore() + h2->getScore() + h3->getScore() + h4->getScore() + h5->getScore();
+	cout << endl << "Accumulated Points: " << sum << endl;
+
 	auto endTime = chrono::steady_clock::now();
-	auto elapsed = chrono::duration_cast<chrono::milliseconds>(endTime - startTime).count(); 
+	auto elapsed = chrono::duration_cast<chrono::milliseconds>(endTime - startTime).count();
 	cout << endl << "Elapsed Time: " << elapsed << " Milliseconds" << endl;
 
 	delete h1;
@@ -72,5 +60,6 @@ int main() {
 	delete h3;
 	delete h4;
 	delete h5;
+	delete Server;
 	return 0;
 }
