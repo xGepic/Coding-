383bb895f37ad17dbb632aeadf8b0f5bbb378b3a
Gepic <75324840+xGepic@users.noreply.github.com>
2021-06-06 19:20:33 +0200

    Update hacker.h

--- 2. Sem/OOP/Neuromancer/hacker.h
+++ 2. Sem/OOP/Neuromancer/hacker.h
@@ -10,20 +10,23 @@ class hacker{
 private:
 	int lifePoints;
 	int attackPoints;
-	int score;
+	int score; 
+	string name;
 	mutex m;
 
 public:
-	hacker(),
+	hacker(string namee),
 	~hacker();
 
 	int getAttackPoints();
 	int getLifePoints();
 	int getScore();
+	string getName();
 
 	void changeScore(int x);
 	void changeAttackPoints(int x);
 	void changeLifePoints(int x);
+	int rng(int x);
 
 	void attack(server* zone, int whichZone);
 };
