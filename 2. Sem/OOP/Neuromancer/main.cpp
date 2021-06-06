bacbc4589d80a692f5ff72b949c49a4fc125a0df
Gepic <75324840+xGepic@users.noreply.github.com>
2021-06-06 19:20:43 +0200

    Update main.cpp

--- 2. Sem/OOP/Neuromancer/main.cpp
+++ 2. Sem/OOP/Neuromancer/main.cpp
@@ -6,11 +6,24 @@
 
 using namespace std;
 
-mutex m;
-
 int rng() {
 
 	return rand() % 5;
+}
+
+void printStatus(hacker* h1, hacker* h2, hacker* h3, hacker* h4, hacker* h5, server* Server) {
+
+	int sum = h1->getScore() + h2->getScore() + h3->getScore() + h4->getScore() + h5->getScore();
+
+	cout << "[STATUS]" << endl;
+	cout << "Hacker1 LP: " << h1->getLifePoints() << "  " << "Hacker1 Points: " << h1->getScore() << endl;
+	cout << "Hacker2 LP: " << h2->getLifePoints() << "  " << "Hacker2 Points: " << h2->getScore() << endl;
+	cout << "Hacker3 LP: " << h3->getLifePoints() << "  " << "Hacker3 Points: " << h3->getScore() << endl;
+	cout << "Hacker4 LP: " << h4->getLifePoints() << "  " << "Hacker4 Points: " << h4->getScore() << endl;
+	cout << "Hacker5 LP: " << h5->getLifePoints() << "  " << "Hacker5 Points: " << h5->getScore() << endl;
+
+	cout << endl << "Server Lifepoints: " << Server->getLifePoints() << endl;
+	cout << "Accumulated Points: " << sum << endl;
 }
 
 int main() {
@@ -21,11 +34,11 @@ int main() {
 	auto startTime = chrono::steady_clock::now();
 
 	server* Server = new server();
-	hacker* h1 = new hacker();
-	hacker* h2 = new hacker();
-	hacker* h3 = new hacker();
-	hacker* h4 = new hacker();
-	hacker* h5 = new hacker();
+	hacker* h1 = new hacker("Hacker1");
+	hacker* h2 = new hacker("Hacker2");
+	hacker* h3 = new hacker("Hacker3");
+	hacker* h4 = new hacker("Hacker4");
+	hacker* h5 = new hacker("Hacker5");
 
 	thread hacker1(&hacker::attack, h1, ref(Server), rng());
 	thread hacker2(&hacker::attack, h2, ref(Server), rng());
@@ -40,20 +53,19 @@ int main() {
 
 	if ((h1->getLifePoints() <= 0) && (h2->getLifePoints() <= 0) && (h3->getLifePoints() <= 0) && (h4->getLifePoints() <= 0) && (h5->getLifePoints() <= 0)) {
 
-		cout << endl << "Server Won!" << endl;
+		cout << endl << endl << "Server Won!" << endl << endl;
+		printStatus(h1, h2, h3, h4, h5, Server);
 	}
 
 	if (Server->getLifePoints() <= 0) {
 
-		cout << endl << "Hackers Won!" << endl;
+		cout << endl << "Hackers Won!" << endl << endl;
+		printStatus(h1, h2, h3, h4, h5, Server);
 	}
-
-	int sum = h1->getScore() + h2->getScore() + h3->getScore() + h4->getScore() + h5->getScore();
-	cout << endl << "Accumulated Points: " << sum << endl;
 
 	auto endTime = chrono::steady_clock::now();
 	auto elapsed = chrono::duration_cast<chrono::milliseconds>(endTime - startTime).count();
-	cout << endl << "Elapsed Time: " << elapsed << " Milliseconds" << endl;
+	cout << endl << endl << "Elapsed Time: " << elapsed << " Milliseconds" << endl;
 
 	delete h1;
 	delete h2;
