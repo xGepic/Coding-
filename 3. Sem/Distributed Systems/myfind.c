774ab8b249ac8d1b5f57e43e13d1c4cd36fffa59
Gepic <75324840+xGepic@users.noreply.github.com>
2021-10-01 21:29:52 +0200

    Update myfind.c

--- 3. Sem/Distributed Systems/myfind.c
+++ 3. Sem/Distributed Systems/myfind.c
@@ -1,25 +1,40 @@
+#include <stdio.h>
 #include <iostream>
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
 #include <assert.h>
 
-char *programName = NULL;
+using namespace std;
 
 //Function printUsage to print out the usage message
 void printUsage()
 {
-    fprintf(stderr, "Usage: %s [-R] [-i] searchpath filename 1 [filename2] ... [filenameN]\n", programName);
-    exit(EXIT_FAILURE);
+    cout << "Usage: ./myfind [-R] [-i] searchpath filename 1 [filename2] ... [filenameN]" << endl;
+    exit(-1);
 }
-
-//Output Format: <pid>: <filename>: <complete-path-to-found-file>\n
 
 int main(int argc, char *argv[])
 {
     int op;
+    unsigned int rFlagCount = 0;
+    unsigned int iFlagCount = 0;
+
+    //Loop through all the flags
     while ((op = getopt(argc, argv, "af:")) != EOF)
     {
-        //todo
+        switch (op)
+        {
+        case 'r':
+            rFlagCount++;
+            break;
+        case 'i':
+            iFlagCount++;
+            break;
+        case '?':
+            printUsage();
+            break;
+        }
     }
+    
 }
