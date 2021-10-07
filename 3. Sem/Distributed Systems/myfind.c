16bab1e80418e0acf52aca642737e3f3009c7a86
Gepic <75324840+xGepic@users.noreply.github.com>
2021-10-07 11:54:35 +0200

    Update myfind.c

--- 3. Sem/Distributed Systems/myfind.c
+++ 3. Sem/Distributed Systems/myfind.c
@@ -5,55 +5,45 @@ using namespace std;
 //Function printUsage to print out the usage message
 void printUsage()
 {
-    cout << "Usage: ./myfind [-R] [-i] searchpath filename 1 [filename2] ...  [filenameN]\n";
+    fprintf(stderr, "Usage: ./myfind [-R] [-i] searchpath filename 1 [filename2] ...  [filenameN]\n");
 }
 
 int main(int argc, char *argv[])
 {
-    pid_t pid; //process id
-    int op;
-    int one = 1;
-    unsigned int rFlagCount = 0;
-    unsigned int iFlagCount = 0;
-    string filenames[argc - optind];
-    string path;
+    pid_t pid;               //process ID
+    char op;                 //operator
+    bool rFlagCount = false; //bool for R Flag
+    bool iFlagCount = false; //bool for i Flag
+    string files[argc - optind]; //string for the filnames, as big as necessary
+    string directory;
 
     //Loop through all the flags
-    while ((op = getopt(argc, argv, "af:")) != EOF)
+    while ((op = getopt(argc, argv, "Ri:")) != EOF)
     {
         switch (op)
         {
-        case 'r':
-            rFlagCount++;
+        case 'R':
+            rFlagCount = true;
             break;
         case 'i':
-            iFlagCount++;
+            iFlagCount = true;
             break;
         case '?':
             printUsage();
-            exit(-1);
+            exit(EXIT_FAILURE);
             break;
         }
     }
-
-    //get directory and files that follow after the flags
+    // get directory and files that follow after the flags
     for (int i = optind; i < argc; i++)
     {
         if (i == optind)
         {
-            path = argv[i];
+            directory = argv[i];
         }
         else
         {
-            filenames[i - optind - 1] = argv[i];
+            files[i - optind - 1] = argv[i];
         }
     }
-
-    //Printing out what the program is going to do
-    cout << "Searching in Directory: '" << path << "' for files: ";
-    for (int i = 0; i < argc - optind - 1; i++)
-    {
-        cout << "'" << filenames[i] << "'";
-    }
-    cout << "\n";
 }
